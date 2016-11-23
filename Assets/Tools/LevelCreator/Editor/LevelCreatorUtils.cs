0b73966d256ffd0aa1bcce807e2d0083e5e94f0a
Ashlee <sweetclimusic@users.noreply.github.com>
2016-11-23 14:47:23 +0000

    Tool to create a empty level with essiental gameobjects.

---
+++ Assets/Tools/LevelCreator/Editor/LevelCreatorUtils.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using UnityEditor.SceneManagement;
+using RunAndJump;
+
+namespace sweetcli.LevelCreator {
+	public static class LevelCreatorUtils {
+		
+
+		 static void CreateScene(){
+			EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo ();
+
+			//create a new empty scene
+			EditorSceneManager.NewScene ( 
+				NewSceneSetup.DefaultGameObjects,NewSceneMode.Single
+			);
+			//same time we create the scene add the first component.
+			GameObject levelScript = new GameObject("Level");
+			levelScript.transform.position = Vector3.zero;
+			levelScript.AddComponent <RunAndJump.Level>();
+		}
+
+		//Obsolete maybe due to using EditorSceneManager instead of how the example does it.
+		//so called from a new menu item now.
+		 static void ClearScene(){
+			GameObject[] objects = Object.FindObjectsOfType<GameObject> ();
+			//remove all gameobjects we found
+			foreach (var item in objects) {
+				//Like Destroy but for the Editor. NICE!
+				GameObject.DestroyImmediate (item);
+			}
+		}
+
+
+		public static void NewLevel(){
+			CreateScene ();
+			//ClearScene ();
+		}
+		public static void ResetLevel(){
+			ClearScene ();
+		}
+
+
+
+	}
+}
---
+++ Assets/Tools/LevelCreator/Editor/MenuItems.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+
+namespace sweetcli.LevelCreator {
+	/// <summary>
+	/// Menu items. store and define all entry points to editor scripts
+	/// </summary>
+	public static class MenuItems {
+		//create a new scene
+		[MenuItem ("Tools/LevelCreator/New Level")]
+		public static void CreateLevel(){
+			LevelCreatorUtils.NewLevel();
+		}
+		public static void ResetLevel(){
+			LevelCreatorUtils.ResetLevel();
+		}
+
+	}
+}
