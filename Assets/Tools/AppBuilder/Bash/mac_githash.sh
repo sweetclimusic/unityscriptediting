02a699b4f3e316971022cd395efe0ddd2289d1e5
Ashlee <sweetclimusic@users.noreply.github.com>
2017-02-03 07:22:37 -0500

    removing old tracked folder

--- Assets/Tools/AppBuilder/Bash/mac_githash.sh
+++
@@ -1,3 +0,0 @@
-#! /usr/bin/env sh
-   SHORT_HASH="$( git log --pretty=format:'%h' -n 1 )"
-   echo $SHORT_HASH
--- Assets/Tools/AppBuilder/Scripts/BuildInfo.Autogenerated.cs
+++
@@ -1,5 +0,0 @@
-namespace AppBuilder {
-	public class BuildInfo {
-		public const string Hash = "697a275";
-		public const string Date = "02/02/2017 17:06:37";
-	}}
